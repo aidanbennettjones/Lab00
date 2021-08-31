d5dd616854f7a6f653dd9772dd987202240aeda4
aidanbennett-jones <aidanbennett-jones@localhost>
2021-08-31 16:08:02 -0400

    Added a main method and two additional Candidate objects. (see bottom)

--- src/voting/Candidate.java
+++ src/voting/Candidate.java
@@ -101,4 +101,20 @@ public class Candidate {
 		}
 	}
 
-}
+	public static void main(String[] args) {
+		// Create a new Candidate object. The new object will be referred to as
+		// candidate1 within the program. The candidate1 object will be initialized so
+		// that the candidate's name is Indira Patel, the candidate's party is
+		// Demopublican, and the candidate's number of votes is 35,400.
+		Candidate candidate1 = new Candidate("Indira Patel", "Demopublican", 35400);
+		Candidate candidate2 = new Candidate("Bob Jones", "Demopublican", 45000);
+		Candidate candidate3 = new Candidate("John Dow", "Demopublican", 50323);
+		// Use method calls and string concatenation to print out the candidate's name
+		// and number of votes.
+		System.out.println(candidate1.getName() + ": " + candidate1.getVotes());
+		System.out.println(candidate2.getName() + ": " + candidate2.getVotes());
+		System.out.println(candidate3.getName() + ": " + candidate3.getVotes());
+	}
+
+	}
+
